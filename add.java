c2f78dae9469d9cee8d7149b0122530d4d2d6d85
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-01-31 08:20:34 +0530

    addition in java
    import java .util.*;
    class add
    {
    public static void main(String args[])
     {
    
     int a,b,c;
     Scanner s=new Scanner(System.in);
     System.out.println("enter first value=");
     a=s.nextInt();
     b=s.nextInt();
     c=a+b;
     System.out.println("value="+c);
     }
    }

---
+++ add.java
@@ -0,0 +1,15 @@
+import java .util.*;
+class add
+{
+public static void main(String args[])
+ {
+
+ int a,b,c;
+ Scanner s=new Scanner(System.in);
+ System.out.println("enter first value=");
+ a=s.nextInt();
+ b=s.nextInt();
+ c=a+b;
+ System.out.println("value="+c);
+ }
+}
