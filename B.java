3758fc3cd71caf2c6daf94c2c6a12887ffcefda5
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-02-02 11:54:14 +0530

    class object in java
    import java.util.*;
    class A
    {
        
        public void Add()
        {
            int v,a,i;
            Scanner s=new Scanner(System.in);
            System.out.println("enter value of v:");
            v=s.nextInt();
            
            System.out.println("enter value of a:");
            a=s.nextInt();
            i=v+a;
    
            System.out.println("Add="+i);
        }
            public void display(String Name,int id)
            {
            System.out.println("Id="+id+"Name="+Name);
    
            
        }
    }
     class B
     {
        public static void main(String args[])
     {
     A s=new A();
     s.Add();
     s.display("vaishu",1506);
     }
     }

---
+++ B.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class A
+{
+    
+    public void Add()
+    {
+        int v,a,i;
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter value of v:");
+        v=s.nextInt();
+        
+        System.out.println("enter value of a:");
+        a=s.nextInt();
+        i=v+a;
+
+        System.out.println("Add="+i);
+    }
+        public void display(String Name,int id)
+        {
+        System.out.println("Id="+id+"Name="+Name);
+
+        
+    }
+}
+ class B
+ {
+    public static void main(String args[])
+ {
+ A s=new A();
+ s.Add();
+ s.display("vaishu",1506);
+ }
+ }
