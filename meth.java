581ad3b8639db28b5d6ba57120cfca287acf284e
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-02-12 15:12:39 +0530

    static keywords(2 method)
    class A
    {
        int id;
        String name;
        Static Strin class1="while loop";
        A (int id,String name)
    
        {
    this.id=id;
    this.name=name;
    Static void change();
    {
        class1="passout student";
    }
        }
        void display()
        {
            System.out.println("Id="+id+\t Name="+name+"\t class="+class1");
        }
    }
    class meth
    {
        public static void main(String args[])
        {
            A.change();
            A e=new A(10001,"vaishu");
            e.display();
            A e1=new A(1002,"vikas");
            e1.display();
        }
    }

---
+++ meth.java
@@ -0,0 +1,31 @@
+class A
+{
+    int id;
+    String name;
+    Static Strin class1="while loop";
+    A (int id,String name)
+
+    {
+this.id=id;
+this.name=name;
+Static void change();
+{
+    class1="passout student";
+}
+    }
+    void display()
+    {
+        System.out.println("Id="+id+\t Name="+name+"\t class="+class1");
+    }
+}
+class meth
+{
+    public static void main(String args[])
+    {
+        A.change();
+        A e=new A(10001,"vaishu");
+        e.display();
+        A e1=new A(1002,"vikas");
+        e1.display();
+    }
+}
