4925ad7415764ceccf734e02c21c5fb7e284d3b9
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2025-02-12 15:18:20 +0530

    static keywords(3 block)
    class A
    {
        Static ;
        {
        
        {
            System.out.println("Static block executed");
        }
        public static void main(String args[])
        {
          System.out.println("class execution");
        }
    }

---
+++ blo.java
@@ -0,0 +1,13 @@
+class A
+{
+    Static ;
+    {
+    
+    {
+        System.out.println("Static block executed");
+    }
+    public static void main(String args[])
+    {
+      System.out.println("class execution");
+    }
+}
